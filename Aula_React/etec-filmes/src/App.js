d3af3095fa8811074a43ba784af91a2f9127ca6e
pedroetec1930 <artoriguscomepao@gmail.com>
2024-08-01 09:11:04 -0300

    'aula1'

---
+++ Aula_React/etec-filmes/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    
+    <>
+      <Header/>
+      <h1>lorem ipsum</h1>
+      <p>lorem ipsum</p>
+    </>
+  );
+
+  
+}
+
+export default App;
